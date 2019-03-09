317f79ced763c042773b4cd9bf03457b982c7ea5
Cobra2 <cobra2@example.com>
2019-03-09 18:42:52 +0700

    upload codingan arduino

---
+++ sketch_mar09a/sketch_mar09a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //int hasil=Serial.read();
+  //if(hasil==1){
+  //  Serial.println("ada");
+  //}
+  //delay(1);
+}
